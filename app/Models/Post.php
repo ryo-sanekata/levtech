ab06e030ee80787be3c5dfa53522fbfd29cd35a7
ryo-sanekata <ryosnkt7190@gmail.com>
2025-01-08 23:44:55 +0000

    4th commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     public function getByLimit(int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
@@ -20,4 +25,6 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+
+    
 }
